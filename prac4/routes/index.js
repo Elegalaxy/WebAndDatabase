a2f506a5c55c6769d1f159d95685b38a8d0662c4
Elegalaxy <yokai.czk@gmail.com>
2021-03-30 07:22:54 +1030

    update loads

--- prac4/routes/index.js
+++ prac4/routes/index.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+var counter = 0;
+router.get('/count', function(req, res, next){
+  counter++;
+  res.send(String(counter));
+});
+
 module.exports = router;
