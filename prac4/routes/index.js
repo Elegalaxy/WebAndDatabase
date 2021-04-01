07843da0d4b4c4d911e4ac9b9f258bb5afc9b7bd
Elegalaxy <yokai.czk@gmail.com>
2021-04-01 11:27:03 +1030

    update

--- prac4/routes/index.js
+++ prac4/routes/index.js
@@ -1,3 +1,4 @@
+const { json } = require('express');
 var express = require('express');
 var router = express.Router();
 
@@ -24,6 +25,29 @@ router.get('/color', function(req, res, next){
   if(ind >= 4) ind = 0;
   console.log(color[ind]);
   res.send(color[ind++]);
+});
+
+var timeList = [];
+router.get('/log.html', function(req, res, next){
+  timeList.push(Date());
+  let t = "";
+  for(let i = 0; i < timeList.length; i++){
+    t += "<li>" + String(timeList[i]) + "</li>";
+  }
+  res.send(`
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Document</title>
+  </head>
+  <body>
+  <ul>${t}</ul>
+  </body>
+  </html>
+  `);
 });
 
 module.exports = router;
