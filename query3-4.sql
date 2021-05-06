6151ded819fe3cf0eb610f474c6feb16a1058d43
Elegalaxy <yokai.czk@gmail.com>
2021-05-06 22:29:42 +0930

    prac 6

--- query3-4.sql
+++
@@ -1,4 +0,0 @@
-SELECT given_name, family_name
-FROM Students, Enrolments
-WHERE Enrolments.subject_code = "COMP SCI 2207"
-AND Enrolments.student_id = Students.student_id;
--- query3-5.sql
+++
@@ -1,5 +0,0 @@
-SELECT given_name, family_name, mark
-FROM Enrolments, Students
-WHERE Enrolments.mark < 50
-AND Enrolments.subject_code = "COMP SCI 2000"
-AND Enrolments.student_id = Students.student_id;
