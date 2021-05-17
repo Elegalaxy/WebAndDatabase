30ea2696d93c8ec8791263ab4589d1e392344d28
Elegalaxy <yokai.czk@gmail.com>
2021-05-17 21:46:51 +0930

    update 3

--- routes/index.js
+++ routes/index.js
@@ -40,4 +40,20 @@ router.post('/combine', function(req, res) {
   res.send(fin);
 });
 
+var express = require('express');
+var app = express();
+app.use(express.json());
+
+var blog = [];
+router.post('/addpost', function(req, res) {
+  blog.push(req.body);
+  res.status(200).end();
+});
+
+router.get('/getposts', function(req, res) {
+  let temp = blog;
+  temp.reverse();
+  res.send(temp);
+});
+
 module.exports = router;
