3bb6f7354a2f9d87a490c524754ad9fa5eb55526
Elegalaxy <yokai.czk@gmail.com>
2021-05-26 20:35:48 +0930

    update 6

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,40 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require("mysql");
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'yokai321',
+  database: 'sakila'
+})
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+router.get('/getTable', function(req, res, next) {
+  let cmd = `SELECT first_name, last_name FROM actor;`;
+  connection.query(cmd, function(err, data, fields) {
+    if (err) throw err;
+    res.json({
+      status: 200,
+      data
+    })
+  });
+});
+
+router.post('/addActor', function(req, res, next) {
+  let cmd = `INSERT INTO actor(first_name, last_name, last_update) VALUES (?);`;
+  let today = new Date();
+  let day = today.getFullYear()+"-"+(today.getMonth()+1)+"-"+today.getDate()+" "+today.getHours()+":"+today.getMinutes()+":"+today.getSeconds();
+  let values = [req.body.first, req.body.last, day];
+  connection.query(cmd, [values], function(err, data, fields) {
+    if (err) throw err;
+    res.json({
+      status: 200
+    })
+  });
 });
 
 module.exports = router;
