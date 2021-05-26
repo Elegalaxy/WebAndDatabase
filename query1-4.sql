7ea5a54089c798bd432780759898ebee8168526d
Elegalaxy <yokai.czk@gmail.com>
2021-05-26 17:08:23 +0930

    update 5

--- query1-4.sql
+++ query1-4.sql
@@ -1,3 +1,4 @@
 SELECT DISTINCT address.address FROM address, store, inventory, film
 WHERE inventory.store_id = store.store_id
-AND film.title = "TWISTED PIRATES";
+AND address.address_id = store.address_id
+AND inventory.film_id = 918;
