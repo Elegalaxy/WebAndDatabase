0027300a0e767c5ece38ea509daa3c5b55ca8d77
Elegalaxy <yokai.czk@gmail.com>
2021-05-26 16:28:48 +0930

    update 3

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require("mysql");
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '1234',
+  database: 'sakila'
+})
+
+connection.connect();
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+connection.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
+  if (err) throw err
+
+  console.log('The solution is: ', rows[0].solution)
+})
+
+connection.end()
 module.exports = router;
